6141b3edd0152dfe41481241592174a16603563a
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2026-01-10 23:02:01 +0900

    feat: healthcheck導入

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import "github.com/gin-gonic/gin"
 
 func main() {
   router := gin.Default()
-  router.GET("/ping", func(c *gin.Context) {
+  router.GET("/health_check", func(c *gin.Context) {
     c.JSON(200, gin.H{
-      "message": "pong",
+      "status": "ok",
     })
   })
   router.Run() // デフォルトで0.0.0.0:8080で待機します
