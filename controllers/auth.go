c52cc785b6aa1cdba2885110f412fdbffc927cce
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2026-01-24 10:04:14 +0900

    feat: ログインの情報をバリデートできるようにする

--- controllers/auth.go
+++ controllers/auth.go
@@ -6,8 +6,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func Register(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-        "data": "register endpointです",
-    })
-}
+	type RegisterInput struct {
+		Username string `json:"username" binding:"required"`
+		Password string `json:"password" binding:"required"`
+	}
+
+	func Register(c *gin.Context) {
+		var input RegisterInput
+
+		if err := c.ShouldBindJSON(&input); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+	
+		c.JSON(http.StatusOK, gin.H{
+			"data": "validated!",
+		})
+	}
