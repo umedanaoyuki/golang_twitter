7d662cdf3871f1f7f27cab592db110a670246a0b
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2026-07-05 07:01:22 +0900

    fix: デーブルの修正

--- db/migrations/000012_create_comment_table.up.sql
+++ db/migrations/000012_create_comment_table.up.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS comments (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     tweet_id INTEGER NOT NULL REFERENCES tweets(id) ON DELETE CASCADE,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    CONSTRAINT comments_user_id_tweet_id_unique UNIQUE (user_id, tweet_id)
+    content VARCHAR(140) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
 CREATE INDEX idx_comments_user_id ON comments(user_id);
